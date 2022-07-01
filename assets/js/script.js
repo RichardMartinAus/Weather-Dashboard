6521ce66947fea98b035636a140f71dc99f2c62a
RichardMartinAUS <richard.martin001@gmail.com>
2022-07-01 16:52:38 +1000

    added TODOs to JS file

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+// add event listeners to search button
+
+// save search in local storage
+
+// create buttons for search history and add event listeners to them
+
+// fetch weather from api and filter it to include: City, Date, Icon, Temp, Wind Speed, Humidity & UV index
+
+// Colour the UV index Green, Yellow or Red
+
+// Fetch 5 day forecast and filter Date, Icon, Temp, Windspeed & Humidity for 5 cards
