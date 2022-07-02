6be4e7d74cc205f843dfcf83ca15fcf00cb6c999
RichardMartinAUS <richard.martin001@gmail.com>
2022-07-02 14:54:51 +1000

    updated html

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,21 @@
-// add event listeners to search button
+// User inputs search into search field
 
-// save search in local storage
+// On click, City name is recorded
 
-// create buttons for search history and add event listeners to them
+// City name written to empty array to store in local storage
+// Name used to append a gray button to the HTML under the search button
 
-// fetch weather from api and filter it to include: City, Date, Icon, Temp, Wind Speed, Humidity & UV index
+// API uses city name to get lat and long ()
+// http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid={API key}
 
-// Colour the UV index Green, Yellow or Red
+// Use lat and long to search OneCall API
+// https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}
+// Get City Name, Date, Icon, Temp, Wind Speed, Humidity & UV index
+// write info to html
+// colour UV index text
+// Fetch info for 5 day forecast
+// Date, Icon, Temp, Wind Speed, Humidity
+// Write to 5 forecast cards in html
 
-// Fetch 5 day forecast and filter Date, Icon, Temp, Windspeed & Humidity for 5 cards
+// Add event listener to History section, use name from button to start search function again.
+
