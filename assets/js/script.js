70da3c92b01c384c85efbfc0983711c12025a3f1
RichardMartinAUS <richard.martin001@gmail.com>
2022-07-02 22:04:31 +1000

    started on the javascript - city search recoreded and history buttons added

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,30 @@
 // User inputs search into search field
+var userInput = document.querySelector("#search-input");
+var searchButton = document.querySelector("#search-button");
+var userHistoryButtons = document.querySelector("#history-buttons");
+
+// funtion logs user city
+function userCitySelected(event) {
+    event.preventDefault();
+    
+    var userCity = userInput.value;
+    console.log(userCity);
+
+    // function to add a button with userCity
+    var button = document.createElement("button");
+    button.setAttribute("class", "btn btn-secondary m-2");
+    button.textContent = userCity.toString();
+
+    var history = document.getElementById("history-buttons")
+    history.appendChild(button);
+}
+
+searchButton.addEventListener("click", userCitySelected);
+
+
 
 // On click, City name is recorded
+
 
 // City name written to empty array to store in local storage
 // Name used to append a gray button to the HTML under the search button
@@ -19,3 +43,14 @@
 
 // Add event listener to History section, use name from button to start search function again.
 
+// function showResponse(event) {
+//     // Prevent default action
+//     event.preventDefault();
+//     console.log(event);
+//     var response = "Thank you for your submission " + nameInput.value + "! We will reach out to you at " + emailInput.value + ".";
+//     submissionResponseEl.textContent = response;
+//   }
+    
+//   // Add listener to submit element
+//   submitEl.addEventListener("click", showResponse);
+  
