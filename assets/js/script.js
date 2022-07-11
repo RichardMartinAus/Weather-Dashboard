3c78e23bb9862f1708bc19a95536a82d4debe454
RichardMartinAUS <richard.martin001@gmail.com>
2022-07-11 12:01:44 +1000

    almost working - a few issues still

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,68 +3,129 @@ var userInput = document.querySelector("#search-input");
 var searchButton = document.querySelector("#search-button");
 var userHistoryButtons = document.querySelector("#history-buttons");
 
-// funtion logs user city
-function getWeather(event) {
+var userLat = -37.81;
+var userLong = 144.96;
+
+// funtion gets user city coordinates to be used for weather search
+function getCoords(event) {
     event.preventDefault();
+
+    var userCityName = userInput.value;
     
-    var userCity = userInput.value;
-    console.log(userCity);
+    // function to add a button with userCity
+    var historyButton = document.createElement("button");
+    historyButton.setAttribute("class", "btn btn-secondary m-2");
+    historyButton.textContent = userCityName.toString();
+
+    var history = document.getElementById("history-buttons")
+    history.appendChild(historyButton);
 
     //API uses city name to get lat and long ()
-    // http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid={API key}
-    fetch('http://api.openweathermap.org/geo/1.0/direct?q=' + userCity + '&limit=1&appid=71b26fc7b3232cb9e9c26358ecffa802', {
-        method: 'GET', //GET is the default.
-        credentials: 'same-origin', // include, *same-origin, omit
-        redirect: 'follow', // manual, *follow, error
-    })
+    
+
+    fetch('http://api.openweathermap.org/geo/1.0/direct?q=' + userCityName + '&limit=1&appid=71b26fc7b3232cb9e9c26358ecffa802', {
+        method: 'GET', 
+        credentials: 'same-origin', 
+        redirect: 'follow', 
+        })
         .then(function (response) {
             return response.json();
         })
         .then(function (data) {
-            console.log(data);
+            userLat = (data[0].lat);
+            userLong = (data[0].lon);
+            
         });
+
+    getWeather(userLat, userLong);
+          
+}
+
+// Use lat and long to search OneCall API
+function getWeather() {
+    var cityName = document.getElementById("city-name");
+    var weatherIcon = document.getElementById("icon")
+    var currentTemp = document.getElementById("current-temp");
+    var windSpeed = document.getElementById("wind-speed");
+    var currentHumidity = document.getElementById("humidity");
+    var uvIndex = document.getElementById("UVI");
+
+    // CARDS
+    var card1 = document.getElementById("card-1");
+    var card2 = document.getElementById("card-2");
+    var card3 = document.getElementById("card-3");
+    var card4 = document.getElementById("card-4");
+    var card5 = document.getElementById("card-5");
     
-        // SAVE LAT AND LONG FROM OBJECT 
-        
-    // Use lat and long to search OneCall API
-    // https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}
-    // Get City Name, Date, Icon, Temp, Wind Speed, Humidity & UV index
-    // write info to html
-    // colour UV index text
 
-    // function to add a button with userCity
-    var button = document.createElement("button");
-    button.setAttribute("class", "btn btn-secondary m-2");
-    button.textContent = userCity.toString();
+    fetch('https://api.openweathermap.org/data/2.5/onecall?lat=' + userLat + '&lon=' + userLong + '&exclude=minutely,hourly,alerts&units=metric&appid=71b26fc7b3232cb9e9c26358ecffa802', {
+        method: 'GET', 
+        credentials: 'same-origin', 
+        redirect: 'follow', 
+        })
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+                        
+            // Weather icon code from StackOverflow https://stackoverflow.com/questions/44177417/how-to-display-openweathermap-weather-icon
+            var iconcode = data.current.weather[0].icon;
+            var iconurl = "http://openweathermap.org/img/wn/" + iconcode + "@2x.png";
 
-    var history = document.getElementById("history-buttons")
-    history.appendChild(button);
+            cityName.innerHTML = ("<h5>" + data.timezone + "  |  " + (moment.unix(data.current.dt).format("DD/MM/YYYY")) + "</h5>");
+            weatherIcon.classList.remove("hide");
+            weatherIcon.innerHTML = ("<div id='icon'><img id='wicon' src='" + iconurl + "' alt='Weather icon'></div>");
+            currentTemp.textContent = ("Temperature: " + data.current.temp + " ˚C");
+            windSpeed.textContent = ("Wind: " + data.current.wind_speed + " meters/sec");
+            currentHumidity.textContent = ("Humidity: " + data.current.humidity + "%");
+            uvIndex.textContent = ("UV Index: " + data.current.uvi);
+            if (data.current.uvi <= 2) {
+                uvIndex.classList.add("uvlow");
+            } else if (data.current.uvi > 2 && data.current.uvi < 6){ 
+                uvIndex.classList.add("uvmed");
+            } else {
+                uvIndex.classList.add("uvhigh");
+            }
 
+            // CARDS 
+            // card1
+            var iconcode2 = data.daily[1].weather[0].icon;
+            var iconurl2 = "http://openweathermap.org/img/wn/" + iconcode2 + ".png";
+
+            card1.innerHTML = ("<p>" + (moment.unix(data.daily[1].dt).format("DD/MM/YYYY")) + "<div id='icon'><img id='wicon' src='" + iconurl2 + "' alt='Weather icon'></div>" + "<br>" + "Temp: " + data.daily[1].temp.day + " ˚C" + "<br>" + "Wind: " + data.daily[1].wind_speed + "<br>" + "Humidity: " +  data.daily[1].humidity + "%");
+
+            // card2
+            var iconcode3 = data.daily[2].weather[0].icon;
+            var iconurl3 = "http://openweathermap.org/img/wn/" + iconcode3 + ".png";
+
+            card2.innerHTML = ("<p>" + (moment.unix(data.daily[2].dt).format("DD/MM/YYYY")) + "<div id='icon'><img id='wicon' src='" + iconurl3 + "' alt='Weather icon'></div>" + "<br>" + "Temp: " + data.daily[2].temp.day + " ˚C" + "<br>" + "Wind: " + data.daily[2].wind_speed + "<br>" + "Humidity: " +  data.daily[2].humidity + "%");
+
+            // card3
+            var iconcode4 = data.daily[3].weather[0].icon;
+            var iconurl4 = "http://openweathermap.org/img/wn/" + iconcode4 + ".png";
+
+            card3.innerHTML = ("<p>" + (moment.unix(data.daily[3].dt).format("DD/MM/YYYY")) + "<div id='icon'><img id='wicon' src='" + iconurl4 + "' alt='Weather icon'></div>" + "<br>" + "Temp: " + data.daily[3].temp.day + " ˚C" + "<br>" + "Wind: " + data.daily[3].wind_speed + "<br>" + "Humidity: " +  data.daily[3].humidity + "%");
+
+            // card4
+            var iconcode5 = data.daily[4].weather[0].icon;
+            var iconurl5 = "http://openweathermap.org/img/wn/" + iconcode5 + ".png";
+
+            card4.innerHTML = ("<p>" + (moment.unix(data.daily[4].dt).format("DD/MM/YYYY")) + "<div id='icon'><img id='wicon' src='" + iconurl5 + "' alt='Weather icon'></div>" + "<br>" + "Temp: " + data.daily[4].temp.day + " ˚C" + "<br>" + "Wind: " + data.daily[4].wind_speed + "<br>" + "Humidity: " +  data.daily[4].humidity + "%");
+
+            // card5
+            var iconcode6 = data.daily[5].weather[0].icon;
+            var iconurl6 = "http://openweathermap.org/img/wn/" + iconcode6 + ".png";
+
+            card5.innerHTML = ("<p>" + (moment.unix(data.daily[5].dt).format("DD/MM/YYYY")) + "<div id='icon'><img id='wicon' src='" + iconurl6 + "' alt='Weather icon'></div>" + "<br>" + "Temp: " + data.daily[5].temp.day + " ˚C" + "<br>" + "Wind: " + data.daily[5].wind_speed + "<br>" + "Humidity: " +  data.daily[5].humidity + "%");
+        });
 
 }
 
-searchButton.addEventListener("click", getWeather);
+// Event listener for the search button
+searchButton.addEventListener("click", getCoords);
 
-
-
-// On click, City name is recorded
-
-
-// City name written to empty array to store in local storage
-// Name used to append a gray button to the HTML under the search button
-
-// API uses city name to get lat and long ()
-// http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid={API key}
-
-// Use lat and long to search OneCall API
-// https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}
-// Get City Name, Date, Icon, Temp, Wind Speed, Humidity & UV index
-// write info to html
-// colour UV index text
-// Fetch info for 5 day forecast
-// Date, Icon, Temp, Wind Speed, Humidity
-// Write to 5 forecast cards in html
-
+// STILL TO DO
+// SAVE SEARCH HISTORY TO LOCAL STORAGE
 // Add event listener to History section, use name from button to start search function again.
 
 
